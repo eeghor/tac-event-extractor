65f4a6d30bed907c840adde52a14a1ccd5383c76
eeghor <eeghor@gmail.com>
2016-09-14 18:40:42 +1000

    rare and common names

--- preprocess_training_data.py
+++ preprocess_training_data.py
@@ -19,25 +19,34 @@ print(kaggle_babynames_df.head(4))
 
 print("found {} unique names in the Kaggle US babynames dataset".format(kaggle_babynames_df["Name"].nunique()))
 
-
-# print(kaggle_babynames_df.groupby('Name','Gender').sum())
-
 df1 = pd.DataFrame(kaggle_babynames_df.groupby(['Name','Gender']).sum()).reset_index()
 
 df1.columns = ["Name","Gender","Count"]
 total_records = df1["Count"].sum()
 
 print(total_records)
-df1.sort(columns="Count",ascending=False,inplace=True)
+df1.sort_values(by="Count",ascending=False,inplace=True)
 df1["Pct"] = df1["Count"].apply(lambda _: np.round(_*100/total_records, 5))
 
 print(df1.head(5))
-print(df1.tail())
+print(df1.tail(5))
 
-print(df1["Pct"].quantile(q=[0.5, 0.95]))
+quantile09 = df1["Pct"].quantile(0.97)
+idx_rare = df1["Pct"] < quantile09
+#print(idx_rare)
+idx_common = df1["Pct"] >= quantile09
+
+rare_names = df1[idx_rare]["Name"].tolist()
+common_names = df1[idx_common]["Name"].tolist()
+
+print("{} rare names".format(len(rare_names)))
+print("{} common names".format(len(common_names)))
+
+print(common_names[:10])
 
 # create a DAWG
-completion_dawg = dawg.CompletionDAWG(kaggle_babynames_df["Name"].str.lower())
+rare_names_dawg = dawg.CompletionDAWG(rare_names)
+common_names_dawg = dawg.CompletionDAWG(common_names)
 
 # check the training dataset for names and replace any identified names with the token NAME
 
@@ -45,7 +54,8 @@ NSENT = len(train_df)
 print("have {} sentences in training dataset".format(NSENT))
 
 for i, sent in enumerate(train_df):
-	sent["ptags"] = [["NAME"] if w.lower() in completion_dawg else [] for w in sent["words"]]
+	sent["ptags"] = [["RARE_NAME"] if w.capitalize() in rare_names_dawg else 
+					["COMMON_NAME"] if w.capitalize() in common_names_dawg else [] for w in sent["words"]]
 
 with open("new_ds.json","w") as f:
 	json.dump(train_df,f)
