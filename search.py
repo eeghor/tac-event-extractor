6a0008393db38cb66cbd6cfa2f24d96c78d075bd
eeghor <eeghor@gmail.com>
2016-09-20 14:50:26 +1000

    updated viterbi

--- search.py
+++ search.py
@@ -3,18 +3,19 @@ this is a base class for search algorithms like Viterbi or Beam search
 """
 class SearchAlgorithm(object):
 
-	START = ["START1","START2"]
-	END = ["END1","END2"]
-
 	def __init__(self, sentence, possible_labels, features_n_weights):
 
 		# sentence is a dict like {"words": [word1, word2, ..], "events":[O,O,..]}
 		# features_n_weights is a dict of features like {f1: weight1,f2: weight2,..}
 
-		self.sentence = sentence
+		self.sentence = sentence 
 		self.labels = possible_labels
 		self.assigned_labels = []  # the best sequence of labels to be found by algorithm
-		self.features_n_weights = features_n_weights
+		self.fw = features_n_weights
 		
 
 
+s=SearchAlgorithm({"words":["this","is","very","interesting"], "POSs":["VV","VB","NN","JJ"]},
+					["Event","Nonevent"], {"f1":12,"f2":3})
+
+print(s.sentence)
--- viterbi.py
+++ viterbi.py
@@ -1,29 +1,35 @@
 from search import SearchAlgorithm
 from collections import defaultdict
+import numpy as np
 
 class Viterbi(SearchAlgorithm):
 
-	def __init__(self,sentence, possible_labels, features_n_weights,iterations):
+	def __init__(self,sentence, possible_labels, fw):
 		
-		SearchAlgorithm.__init__(self,sentence, possible_labels, features_n_weights)
-		self.iterations = iterations
-		self._sent_count = 0
+		SearchAlgorithm.__init__(self,sentence, possible_labels, fw)
+		self._NW = len(sentence)
+		self._NL = len(possible_labels)
+		self._vit_matrix = np.zeros(shape=(self._NL,self._NW +2))
+		self._backpointer_matrix = np.zeros(shape=(self._NL,self._NW))
+		self.possible_labels = ["START"]+possible_labels+["END"]
 
-	def _label_words(self):
+	def _run_viterbi_algorithm(self):
 
-		scores = []  # list of dicts like [{lab1:12, lab2:0,...}, {lab1:33,..}], i-th word corresp. i-th dict
+		for j, label in enumerate(possible_labels, 1):
+			self._vit_matrix[j-1, 0] = fw["word i + event i: [{}]+[{}]".format(self.sentence["words"][0],self.sentence["events"][j])+\
+										fw["event i-1 -> event i: [{}]->[{}]".format(self.sentence["events"][j-1],self.sentence["events"][j])
 
 		for i, w in enumerate(self.sentence["words"]):
 
-			i += 2
+			for j, label in enumerate(possible_labels):
 
-			for label in possible_labels:
+				self._vit_matrix[j, 0] = fw["word i + event i: [{}]+[{}]".format(self.sentence["words"][i],self.sentence["events"][i])
 				
-				score_this_word = defaultdict(int)
-				score_this_word[label] = features_n_weights["event i-1 -> event i: [{}]->[{}]".format(self.sentence["events"][i-1],self.sentence["events"][i])]+\
-				features_n_weights["word i + event i: [{}]+[{}]".format(self.sentence["words"][i],self.sentence["events"][i])]
+				self._vit_matrix[j,i] = self._vit_matrix[,i-1] fw["event i-1 -> event i: [{}]->[{}]".format(self.sentence["events"][i-1],self.sentence["events"][i])]+\
+										fw["word i + event i: [{}]+[{}]".format(self.sentence["words"][i],self.sentence["events"][i])]
 
-			scores.append(score_this_word)
+			# by now we filled out the i-th column; now time to find
+
 
 
 
