3492d20fdd32544dd626a013f3933648ecb27864
eeghor <eeghor@gmail.com>
2016-09-21 00:38:57 +1000

    search

--- viterbi.py
+++ viterbi.py
@@ -10,7 +10,7 @@ class Viterbi(SearchAlgorithm):
 		self._NW = len(sentence)
 		self._NL = len(possible_labels)
 		self._vit_matrix = np.zeros(shape=(self._NL,self._NW +2))
-		self._backpointer_matrix = np.zeros(shape=(self._NL,self._NW))
+		self._backpointer_matrix = np.zeros(shape=(self._NL,self._NW+1))
 		self.possible_labels = possible_labels
 
 	def _run_viterbi_algorithm(self):
@@ -19,21 +19,31 @@ class Viterbi(SearchAlgorithm):
 		for j, label in enumerate(possible_labels):
 			self._vit_matrix[j, 0] = fw["(ev-1):[{}]->(ev)[{}]".format("START",self.sentence["events"][j]+\
 										fw["(ev):[{}]=>(word)[{}]".format(self.sentence["events"][j],self.sentence["words"][0])
+			self._backpointer_matrix[j,0] = -1
 		# for all other columns, i.e. second word to the last word
 		for i, w in enumerate(self.sentence["words"][1:],1):
 			for j, label in enumerate(possible_labels):
 				# introduce a temporary storage for scores: a list [(scores if coming from l1), (scores if coming from l2),...]
 				scores = []
 				# suppose label has been placed on word; then run through all the labels and calculate scores
-				for 
-				self._vit_matrix[j, 0] = fw["word i + event i: [{}]+[{}]".format(self.sentence["words"][i],self.sentence["events"][i])
-				
-				self._vit_matrix[j,i] = self._vit_matrix[,i-1] fw["event i-1 -> event i: [{}]->[{}]".format(self.sentence["events"][i-1],self.sentence["events"][i])]+\
-										fw["word i + event i: [{}]+[{}]".format(self.sentence["words"][i],self.sentence["events"][i])]
+				for l, lb in enumerate(possible_labels):
+					scores.append(self._vit_matrix[l, i-1] +\
+						fw["(ev-1):[{}]->(ev)[{}]".format(lb,label)+fw["(ev):[{}]=>(word)[{}]".format(label,w)
+				# find maximum score and its index
+				highest_score = max(scores)
+				index_highest_score = scores.index(highest_score)
+				self._vit_matrix[j, i] = highest_score
+				self._backpointer_matrix[j,i] = index_highest_score
+		# finally, the very last imaginary termination
+		scores = []
+		for j, label in enumerate(possible_labels):
+			scores.append(self._vit_matrix[j, -1] +\
+				fw["(ev-1):[{}]->(ev)[{}]".format(label,"END").format(label,w)
 
-			# by now we filled out the i-th column; now time to find
+		highest_score = max(scores)
+		index_highest_score = scores.index(highest_score)
 
-
+		for 
 
 
 			
