9bc21516d7da77922a4b8b939f684ab5c0dc71ad
eeghor <eeghor@gmail.com>
2016-09-21 17:30:21 +1000

    improvements

--- features.py
+++ features.py
@@ -6,129 +6,58 @@ EXTRACT FEATURES FROM A WORD.
 * can either extract all fetures or only Markov features needed for the Viterbi algorithm
 """
 
-class wFeatures(object):
+class FeatureFactory(object):
 
-	def __init__(self, sent, what_features='all', word_idx=None, nomlex_dict=None):
+	def __init__(self, sent, word_idx, nomlex_dict=None):  # Nomlex dictionary is optional
 
-		self.sent = sent  		  # sentence
-		self.word_idx = word_idx  # word index: None to extract from all words, int index otherwise
-		self.feature_dict = defaultdict(int)
+		self.sentence = {k: ["START1","START2"] + v +["END1","END2"] for k,v, in sent.items()}   # sentence
+		self.word_idx = word_idx  		
 		self.nomlex_dict = nomlex_dict
-		self.what_features = what_features
+
+		self.feature_dict = defaultdict(int)
 
 	def add(self, name, weight):
 		self.feature_dict[name] += weight
 
-	def extract(self):
+	def extract(self):  # extract features from a word
 
-		if self.word_idx is None:
-			wi = range(len(self.sent["words"]))
-			print("wi=",wi)
-		else:
-			wi = range(self.word_idx,self.word_idx+1)
+		i = self.word_idx + 2  # because now  we have START1, START2, word1, ..
+		__pidx = i - 1  # previous index
+		__nidx = i + 1  # next index
 
-		for i in wi:
+		# transition features
+		self.add("(word-1):[{}]->(word)[{}]".format(self.sentence["words"][__pidx],self.sentence["words"][i]), 1)
+		self.add("(lem-1):[{}]->(lem)[{}]".format(self.sentence["lemmas"][__pidx],self.sentence["lemmas"][i]), 1)
+		self.add("(lem-2):[{}]->(lem-1)[{}]->(lem)[{}]".format(self.sentence["lemmas"][i-2], self.sentence["lemmas"][__pidx],self.sentence["lemmas"][i]), 1)
+		self.add("(pos-1):[{}]->(pos)[{}]".format(self.sentence["POSs"][__pidx],self.sentence["POSs"][i]), 1)
+		self.add("(ent-1):[{}]->(ent)[{}]".format(self.sentence["entities"][__pidx],self.sentence["entities"][i]), 1)
+		self.add("(ev-1):[{}]->(ev)[{}]".format(self.sentence["events"][__pidx],self.sentence["events"][i]), 1)
 
-			self.__pidx = i - 1
-			self.__nidx = i + 1
+		self.add("(word):[{}]->(word+1)[{}]".format(self.sentence["words"][i], self.sentence["words"][__nidx]), 1)
+		self.add("(lem):[{}]->(lem+1)[{}]".format(self.sentence["lemmas"][i], self.sentence["lemmas"][__nidx]), 1)
+		self.add("(pos):[{}]->(pos+1)[{}]".format(self.sentence["POSs"][i], self.sentence["POSs"][__nidx]), 1)
+		self.add("(ent):[{}]->(ent+1)[{}]".format(self.sentence["entities"][i], self.sentence["entities"][__nidx]), 1)
+		self.add("(ev):[{}]->(ev+1)[{}]".format(self.sentence["events"][i], self.sentence["events"][__nidx]), 1)
 
-			if i == 0:  # if this is the 1st word in sentence
-				self.prev_word = self.prev_pos = self.prev_lemma = self.prev_entity = self.prev_event = "<s>"
-				self.prev2_word = self.prev2_pos = self.prev2_lemma = self.prev2_entity = self.prev2_event = None
-			elif i == 1:
-				self.prev_word = self.sent["words"][self.__pidx]
-				self.prev_lemma = self.sent["lemmas"][self.__pidx]
-				self.prev_pos = self.sent["POSs"][self.__pidx]
-				self.prev_entity = self.sent["entities"][self.__pidx]
-				self.prev_event = self.sent["events"][self.__pidx]
-				self.prev2_word = self.prev2_pos = self.prev2_lemma = self.prev2_entity = self.prev2_event = "<s>"
-			elif i == len(self.sent)-1:
-				self.next_word = self.next_pos = self.next_lemma = self.next_entity = self.next_event = "<e>"
-				self.next2_word = self.next2_pos = self.next2_lemma = self.next2_entity = self.next2_event = None
-				self.prev_word = self.sent["words"][self.__pidx]
-				self.prev_lemma = self.sent["lemmas"][self.__pidx]
-				self.prev_pos = self.sent["POSs"][self.__pidx]
-				self.prev_entity = self.sent["entities"][self.__pidx]
-				self.prev_event = self.sent["events"][self.__pidx]
-			elif i == len(self.sent)-2:
-				self.next2_word = self.next2_pos = self.next2_lemma = self.next2_entity = self.next2_event = "<e>"
-				self.next_word = self.sent["words"][self.__nidx]
-				self.next_lemma = self.sent["lemmas"][self.__nidx]
-				self.next_pos = self.sent["POSs"][self.__nidx]
-				self.next_entity = self.sent["entities"][self.__nidx]
-				self.next_event = self.sent["events"][self.__nidx]
-				self.prev_word = self.sent["words"][self.__pidx]
-				self.prev_lemma = self.sent["lemmas"][self.__pidx]
-				self.prev_pos = self.sent["POSs"][self.__pidx]
-				self.prev_entity = self.sent["entities"][self.__pidx]
-				self.prev_event = self.sent["events"][self.__pidx]
-			else:
-				self.prev_word = self.sent["words"][self.__pidx]
-				self.prev_lemma = self.sent["lemmas"][self.__pidx]
-				self.prev_pos = self.sent["POSs"][self.__pidx]
-				self.prev_entity = self.sent["entities"][self.__pidx]
-				self.prev_event = self.sent["events"][self.__pidx]
-				self.next_word = self.sent["words"][self.__nidx]
-				self.next_lemma = self.sent["lemmas"][self.__nidx]
-				self.next_pos = self.sent["POSs"][self.__nidx]
-				self.next_entity = self.sent["entities"][self.__nidx]
-				self.next_event = self.sent["events"][self.__nidx]
+		# emission features: in HMM, likelihood that hidden event i generated the observed word i
+		self.add("(ev):[{}]=>(word)[{}]".format(self.sentence["events"][i],self.sentence["words"][i]), 1)
 
-			if self.what_features in ['all','markov']:
-				self.feature_dict["word transition [{} -> {}]".format(self.prev_word, self.sent["words"][i])] += 1
-				self.feature_dict["lemma transition [{} -> {}]".format(self.prev_lemma, self.sent["lemmas"][i])] += 1
-				self.feature_dict["pos transition [{} -> {}]".format(self.prev_pos, self.sent["POSs"][i])] += 1
-				self.feature_dict["entity transition [{} -> {}]".format(self.prev_entity, self.sent["entities"][i])] += 1
-				self.feature_dict["event transition [{} -> {}]".format(self.prev_event, self.sent["events"][i])] += 1
-				# emission features
-				self.feature_dict["event word emission [{} -> {}]".format( self.sent["events"][i], self.sent["words"][i])] += 1
+		# lemma synonyms and hypernyms from WordNet
+		if wordnet.synsets(self.sentence["lemmas"][i]):
+		    for w in wordnet.synsets(self.sentence["lemmas"][i].lower()):
+		        for ln in w.lemma_names():
+		            self.add("(syn):[{}]".format(ln),1)
+		        for g in w.hypernyms():
+		            for l in g.lemma_names():
+		            	self.add("(hyp):[{}]".format(l),1)	
 
-			if self.what_features in ['all']:
-				# lemma synonyms and hypernyms from WordNet
-				if wordnet.synsets(self.sent["lemmas"][i]):
-				    for w in wordnet.synsets(self.sent["lemmas"][i].lower()):
-				        for ln in w.lemma_names():
-				            self.feature_dict["wordnet lemma synonym [{}]".format(ln)] += 1
-				        for g in w.hypernyms():
-				            for l in g.lemma_names():
-				                self.feature_dict["wordnet lemma hypernym [{}]".format(l)] += 1		
+		# last 2 letters in word
+		if len(self.sentence["words"][i]) > 4:
+			self.add("(word_end2))[{}]".format(self.sentence["words"][i][-2:]), 1)
 
-				# sub-word features (applied to words)
-				cc = 0
-				for char in self.sent["words"][i]:
-					cc += 1
-					if cc == 1:
-						self.feature_dict["first letter in word [{}]".format(self.sent["words"][i][0])] += 1
-					if cc == 2:
-						self.feature_dict["last 2 letters in word [{}]".format(self.sent["words"][i][:2])] += 1
-					if cc == 3:
-						self.feature_dict["last 3 letters in word [{}]".format(self.sent["words"][i][:3])] += 1
-
-				self.feature_dict["word length [{}]".format(len(self.sent["words"][i]))] += 1
-
-	def __len__(self):
-		return len(self.feature_dict)
-		
-	def get_longest_keylength(self):
-		return len(max(self.feature_dict, key = len))
-
-	def __getitem__(self, ki):
-		return self.feature_dict[ki]
-	
-	def __str__(self):
-		kl = self.get_longest_keylength()
-		st = "".join(["{0:<%d}"%kl,"\t{1:}"]).format("feature","weight")
-		for k, v in sorted(self.feature_dict.items(), key=lambda x: x[1], reverse=True):
-			st = "\n".join([st, "".join(['{0:<%d}'%kl,'\t{1:}']).format(k,v)])
-		return st
-
-	# def __add__(self, other):
-	# 	return wFeatures(self.sent, self.word_idx, defaultdict(int,{**self.feature_dict, **other.feature_dict}))
+		return self.feature_dict
 
 
-
-s = wFeatures({"words":["dynamo", "floated", "in", "the", "sea."], "lemmas":["dynamo", "floated", "in", "the", "sea."],
-"entities":["dynamo", "floated", "in", "the", "sea."], "events":["O", "swim", "O", "O", "O"],"POSs":["N", "fB", "V", "E", "P"]},'markov',3)
-s.extract()
-
-print(s)
+s = FeatureFactory({"words":["dynamo", "floated", "in", "the", "sea."], "lemmas":["dynamo", "floated", "in", "the", "sea."],
+"entities":["dynamo", "floated", "in", "the", "sea."], "events":["O", "swim", "O", "O", "O"],"POSs":["N", "fB", "V", "E", "P"]},0)
+print(s.extract())
--- search.py
+++ search.py
@@ -1,12 +1,12 @@
 """
-this is a base class for search algorithms like Viterbi or Beam search
+this is a base class for search algorithms like Viterbi or Beam search;
 """
 class SearchAlgorithm(object):
 
 	def __init__(self, sentence, possible_labels, features_n_weights):
 
 		# sentence is a dict like {"words": [word1, word2, ..], "events":[O,O,..]}
-		# features_n_weights is a dict of features like {f1: weight1,f2: weight2,..}
+		# features_n_weights is a defaultdict of features like {f1: weight1,f2: weight2,..}
 
 		self.sentence = sentence 
 		self.labels = possible_labels
